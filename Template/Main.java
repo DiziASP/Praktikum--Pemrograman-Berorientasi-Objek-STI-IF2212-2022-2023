bee1b90e1b1c87094a5c3c0d8cce8766fb350b8f
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-01-30 23:39:07 +0700

    lagi

---
+++ Template/Main.java
@@ -0,0 +1,16 @@
+/*
+  Nama      : Raden Dizi Assyafadi Putra
+  NIM       : 18221155
+  Deskripsi : Driver (...isi sendiri)
+*/
+
+/* Import Libraries */
+import java.util.Scanner;
+import java.lang.System;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+    }
+}
