2f5d1e1f4c0e77aecb542739fe48bebed4f2642c
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-01-30 23:31:12 +0700

    initial commit

---
+++ Prak 0/Kali2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println(x * 2);
+        scanner.close();
+    }
+}
